17a2b06018c453c31199885041407b60e3aa59d8
Lance Olsen <lance@squareup.com>
2024-12-27 17:13:51 -0800

    wip

--- ext/or-tools/extconf.rb
+++ ext/or-tools/extconf.rb
@@ -11,29 +11,33 @@ $CXXFLAGS << " -Wno-sign-compare -Wno-ignored-qualifiers -Wno-unused-parameter -
 # hide Rice warnings
 $CXXFLAGS << " -Wno-implicit-fallthrough"
 
-inc, lib = dir_config("or-tools")
-if inc || lib
-  inc ||= "/usr/local/include"
-  lib ||= "/usr/local/lib"
-  rpath = lib
-else
-  # download
-  require_relative "vendor"
-
-  inc = "#{$vendor_path}/include"
-  lib = "#{$vendor_path}/lib"
-
-  # make rpath relative
-  # use double dollar sign and single quotes to escape properly
-  rpath_prefix = RbConfig::CONFIG["host_os"].match?(/darwin/) ? "@loader_path" : "$$ORIGIN"
-  rpath = "'#{rpath_prefix}/../../tmp/or-tools/lib'"
-end
+inc = "/opt/or-tools/include"
+lib = "/opt/or-tools/lib64"
+rpath = lib
+#inc, lib = dir_config("or-tools")
+#if inc || lib
+#  inc ||= "/usr/local/include"
+#  lib ||= "/usr/local/lib"
+#  rpath = lib
+#else
+#  # download
+#  require_relative "vendor"
+#
+#  inc = "#{$vendor_path}/include"
+#  lib = "#{$vendor_path}/lib"
+#
+#  # make rpath relative
+#  # use double dollar sign and single quotes to escape properly
+#  rpath_prefix = RbConfig::CONFIG["host_os"].match?(/darwin/) ? "@loader_path" : "$$ORIGIN"
+#  rpath = "'#{rpath_prefix}/../../tmp/or-tools/lib'"
+#end
 
 # find_header and find_library first check without adding path
 # which can cause them to find system library
 $INCFLAGS << " -I#{inc}"
 # could support shared libraries for protobuf and abseil
 # but keep simple for now
+puts "looking for libprotobuf in #{lib}"
 raise "libprotobuf.a not found" unless File.exist?("#{lib}/libprotobuf.a")
 $LDFLAGS.prepend("-Wl,-rpath,#{rpath} -L#{lib} #{lib}/libprotobuf.a ")
 raise "OR-Tools not found" unless have_library("ortools")
